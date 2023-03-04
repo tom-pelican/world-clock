0899290a051c7edb6aad5736c3138fbff0f51ae2
tom-pelican <117576777+tom-pelican@users.noreply.github.com>
2023-03-04 19:22:12 +0800

    JS update time every second and made possible to display all timezones on one page

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,7 +3,7 @@ function updateTime() {
   let cityNameElement = cityElement.querySelector(".city-name");
   let dateElement = cityElement.querySelector(".date");
   let timeElement = cityElement.querySelector(".time");
-  let cityGuess = moment.tz.guess();
+  let cityGuess = moment.tz.guess().replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityGuess);
 
   cityNameElement.innerHTML = cityGuess;
@@ -15,18 +15,39 @@ updateTime();
 setInterval(updateTime, 1000);
 
 function changeCity(event) {
-  let cityTimeZone = "";
+  setInterval(() => {
+    let cityTimeZone = event.target.value;
 
-  if (event.target.value.length > 0) {
-    cityTimeZone = event.target.value;
-  } else {
-    cityTimeZone = moment.tz.guess();
-  }
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
 
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = "";
+
+    if (cityTimeZone === "all") {
+      let selectElement = document.querySelectorAll("[name=select]");
+      let optionValues = [...selectElement[0].options].map((o) => o.value);
+      optionValues = optionValues.slice(1, -1);
+      optionValues.forEach(function appendTimeZone(cityTimeZone) {
+        let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+        let cityTime = moment().tz(cityTimeZone);
+        citiesElement.innerHTML += `
+          <div class="city" id="city">
+            <div>
+              <h2 class="city-name">${cityName}</h2>
+              <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format(
+              "h:mm:ss [<small>]A[</small>]"
+            )}</div>
+          </div>
+        `;
+      });
+    } else {
+      let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+      let cityTime = moment().tz(cityTimeZone);
+      citiesElement.innerHTML = `
         <div class="city" id="city">
           <div>
             <h2 class="city-name">${cityName}</h2>
@@ -36,7 +57,9 @@ function changeCity(event) {
             "h:mm:ss [<small>]A[</small>]"
           )}</div>
         </div>
-  `;
+      `;
+    }
+  }, 1000);
 }
 
 let citySelectElement = document.querySelector("#city-select");
